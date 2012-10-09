441d0fdff42a227b4863a367ec27687b8aad1142
Patrick Böhner <patrick@bohner.nl>
2012-10-09 12:24:09 +0200

    AirMarshal aangemaakt
    AirMarshal klasse is aangemaakt, Hibernate-mapping deels uitgevoerd en
    toegevoegd aan hibernate.cfg.xml

---
+++ src/Model/AirMarshal.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Patrick
+ */
+public class AirMarshal extends Staff {
+    int badgeNumber;
+    String nationality;
+    
+    public AirMarshal(int badgeNumber, String nationality, int number, PersonalType type, String name, Airport primaryAirport) {
+	super(number, type, name, primaryAirport);
+	this.badgeNumber = badgeNumber;
+	this.nationality = nationality;
+    }
+
+    public int getBadgeNumber() {
+	return badgeNumber;
+    }
+
+    public void setBadgeNumber(int badgeNumber) {
+	this.badgeNumber = badgeNumber;
+    }
+
+    public String getNationality() {
+	return nationality;
+    }
+
+    public void setNationality(String nationality) {
+	this.nationality = nationality;
+    }
+    
+    @Override
+    public String toString() {
+        return super.getName();
+    }
+}
