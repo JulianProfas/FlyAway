f5c64b7bbb105183b76fcee976de9a00a0d6ff91
Patrick Böhner <patrick@bohner.nl>
2012-10-09 13:38:17 +0200

    Nationaliteiten ENUM toegevoegd
    ENUM voor Nationaliteiten toegevoegd (voor AirMarshals).

---
+++ src/Model/Nationality.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template,  choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Patrick
+ */
+public enum Nationality {
+    Afghan, Albanian, Algerian, American, Andorran, Angolan, Antiguans, Argentinean, Armenian, Australian, Austrian, Azerbaijani, Bahamian, Bahraini, Bangladeshi, Barbadian, Barbudans, Batswana, Belarusian, Belgian, Belizean, Beninese, Bhutanese, Bolivian, Bosnian, Brazilian, British, Bruneian, Bulgarian, Burkinabe, Burmese, Burundian, Cambodian, Cameroonian, Canadian, Cape_Verdean, Central_African, Chadian, Chilean, Chinese, Colombian, Comoran, Congolese, Costa_Rican, Croatian, Cuban, Cypriot, Czech, Danish, Djibouti, Dominican, Dutch, East_Timorese, Ecuadorean, Egyptian, Emirian, Equatorial_Guinean, Eritrean, Estonian, Ethiopian, Fijian, Filipino, Finnish, French, Gabonese, Gambian, Georgian, German, Ghanaian, Greek, Grenadian, Guatemalan, Guinea_Bissauan, Guinean, Guyanese, Haitian, Herzegovinian, Honduran, Hungarian, I_Kiribati, Icelander, Indian, Indonesian, Iranian, Iraqi, Irish, Israeli, Italian, Ivorian, Jamaican, Japanese, Jordanian, Kazakhstani, Kenyan, Kittian_and_Nevisian, Kuwaiti, Kyrgyz, Laotian, Latvian, Lebanese, Liberian, Libyan, Liechtensteiner, Lithuanian, Luxembourger, Macedonian, Malagasy, Malawian, Malaysian, Maldivan, Malian, Maltese, Marshallese, Mauritanian, Mauritian, Mexican, Micronesian, Moldovan, Monacan, Mongolian, Moroccan, Mosotho, Motswana, Mozambican, Namibian, Nauruan, Nepalese, New_Zealander, Ni_Vanuatu, Nicaraguan, Nigerian, Nigerien, North_Korean, Northern_Irish, Norwegian, Omani, Pakistani, Palauan, Panamanian, Papua_New_Guinean, Paraguayan, Peruvian, Polish, Portuguese, Qatari, Romanian, Russian, Rwandan, Saint_Lucian, Salvadoran, Samoan, San_Marinese, Sao_Tomean, Saudi, Scottish, Senegalese, Serbian, Seychellois, Sierra_Leonean, Singaporean, Slovakian, Slovenian, Solomon_Islander, Somali, South_African, South_Korean, Spanish, Sri_Lankan, Sudanese, Surinamer, Swazi, Swedish, Swiss, Syrian, Taiwanese, Tajik, Tanzanian, Thai, Togolese, Tongan, Trinidadian_or_Tobagonian, Tunisian, Turkish, Tuvaluan, Ugandan, Ukrainian, Uruguayan, Uzbekistani, Venezuelan, Vietnamese, Welsh, Yemenite, Zambian, Zimbabwean;
+}
